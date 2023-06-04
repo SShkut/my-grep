452569b0b306cdbd8ec80108a84fc80b3a098e60
= <=>
2023-06-04 16:29:12 +0200

    Added file_name argument

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
 use clap::Parser;
 use regex::Regex;
 
 #[derive(Parser, Debug)]
 struct Args {
     search_pattern: String,
+    file_name: String,
 }
 
 fn main() {
     let args = Args::parse();
 
     let search_term = Regex::new(&args.search_pattern).unwrap();
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let file = File::open(&args.file_name);
 
-    for (line_number, line) in quote.lines().enumerate() {
-        let contains_pattern = search_term.find(line);
+    let text = match file {
+        Ok(file) => BufReader::new(file),
+        Err(e) => panic!("Cannot open a file: {}\\n{}", &args.file_name, e),
+    };
+
+    for (line_number, line) in text.lines().enumerate() {
+        let line = match line {
+            Ok(result) => result,
+            Err(_) => String::from(""),
+        };
+
+        let contains_pattern = search_term.find(&line);
 
         match contains_pattern {
             Some(_) => println!("{}, {}", line_number, line),
