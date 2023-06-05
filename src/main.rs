e4cbb4e9cfbdbb26e945b174e4e1463bdc974f79
= <=>
2023-06-05 20:58:55 +0200

    Added stdin

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,63 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
-use clap::Parser;
-use regex::Regex;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-struct Args {
+#[command(name = "my-grep", version)]
+struct Cli {
+    #[command(subcommand)]
+    input: Option<Input>,
+
     search_pattern: String,
-    file_name: String,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Subcommand, Debug)]
+enum Input {
+    FileArg {
+        file_name: String,
+    },
+}
 
-    let search_term = Regex::new(&args.search_pattern).unwrap();
-    let file = File::open(&args.file_name);
-
-    let text = match file {
-        Ok(file) => BufReader::new(file),
-        Err(e) => panic!("Cannot open a file: {}\\n{}", &args.file_name, e),
-    };
-
-    for (line_number, line) in text.lines().enumerate() {
+fn process_lines<T: BufRead + Sized>(reader: T, search_pattern: &str) {
+    for (line_number, line) in reader.lines().enumerate() {
         let line = match line {
             Ok(result) => result,
             Err(_) => String::from(""),
         };
-
-        let contains_pattern = search_term.find(&line);
-
-        match contains_pattern {
-            Some(_) => println!("{}, {}", line_number, line),
-            None => (),
+        let contains_pattern = line.contains(&search_pattern);
+        if contains_pattern {
+            println!("{}, {}", line_number + 1, line);
         }
     }
 }
+
+fn main() {
+    let args = Cli::parse();
+
+    match &args.input {
+        Some(Input::FileArg { file_name }) => find_in_file(&file_name, &args.search_pattern),
+        None => find_in_std_in(&args.search_pattern),
+    }
+}
+
+fn find_in_file(file_name: &str, search_pattern: &str) {
+    let file = File::open(file_name);
+
+    let text = match file {
+        Ok(file) => BufReader::new(file),
+        Err(e) => panic!("Cannot open a file: {}\\n{}", file_name, e),
+    };
+
+    process_lines(text, &search_pattern)
+}
+
+fn find_in_std_in(search_pattern: &str) {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+
+    process_lines(reader, &search_pattern)
+}
+
